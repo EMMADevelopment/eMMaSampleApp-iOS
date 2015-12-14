f4df85fadbe0b37b28bd4756555bb5a29f275958
Adrián Carrera <acarrera@moddity.net>
2015-12-14 13:39:31 +0100

    Initial Commit

---
+++ eMMaSampleApp-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eMMaSampleApp-iOS
+//
+//  Created by Adrian on 14/12/15.
+//  Copyright © 2015 eMMa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eMMaSampleApp-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eMMaSampleApp-iOS
+//
+//  Created by Adrian on 14/12/15.
+//  Copyright © 2015 eMMa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
